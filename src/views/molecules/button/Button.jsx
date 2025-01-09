32a6a8a4d9f29d1fdfd82d24ca5150b40b1c3dc1
Anthony Z <118206086+2valide@users.noreply.github.com>
2025-01-09 11:36:13 +0100

    improve button add size

--- src/views/molecules/button/Button.jsx
+++ src/views/molecules/button/Button.jsx
@@ -3,43 +3,41 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import './button.css';
-import { Icons } from "../../atoms/icons/Icons.jsx";
+import {Icons} from "../../atoms/icons/Icons.jsx";
 
-export const Button = ({ mode, label, ...props }) => {
-  let buttonClass;
+export const Button = ({mode, label, size, ...props}) => {
+    let buttonClass;
 
-  switch (mode) {
-    case 'primary':
-      buttonClass = 'storybook-button--primary';
-      break;
-    case 'secondary':
-      buttonClass = 'storybook-button--secondary';
-      break;
-    case 'danger':
-      buttonClass = 'storybook-button--danger';
-      break;
-    default:
-      buttonClass = 'storybook-button--danger';
-  }
+    switch (mode) {
+        case 'primary':
+            buttonClass = 'storybook-button--primary';
+            break;
+        case 'danger':
+            buttonClass = 'storybook-button--danger';
+            break;
+        default:
+            buttonClass = 'storybook-button--danger';
+    }
 
-  return (
-    <button
-      type="button"
-      className={['storybook-button', buttonClass].join(' ')}
-      {...props}
-    >
-      {mode === "danger" ? <Icons name={"trash"} size={16} /> : undefined}
-      {label}
-    </button>
-  );
+    return (
+        <button
+            type="button"
+            className={['storybook-button', `storybook-button--${size}`, buttonClass].join(' ')}
+            {...props}
+        >
+            {mode === "danger" ? <Icons name={"trash"} size={16}/> : undefined}
+            {label}
+        </button>
+    );
 };
 
 Button.propTypes = {
-  mode: PropTypes.oneOf(['primary', 'secondary', 'danger']),
-  label: PropTypes.string.isRequired,
+    mode: PropTypes.oneOf(['primary', 'danger']),
+    label: PropTypes.string.isRequired,
+    size: PropTypes.oneOf(['small', 'medium', 'large']),
 };
 
 Button.defaultProps = {
-  mode: 'primary',
-  onClick: undefined,
+    mode: 'primary',
+    size: 'medium',
 };
