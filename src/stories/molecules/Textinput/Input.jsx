5058d5f028fd45918531a4fd279a516fc82f0997
Flo738 <pacardflo02@gmail.com>
2025-01-09 13:16:07 +0100

    nouvelles modifications avec hide/show

---
+++ src/stories/molecules/Textinput/Input.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import PropTypes from "prop-types";
+import './input.css';
+
+export const Input = ({type, label, required, error, onClick, ...props}) => {
+
+    const [showPassword, setShowPassword] = React.useState(false);
+
+    const mode = error ? 'storybook-input--error' : 'storybook-input--primary';
+
+    const passwordtoggle = () => {
+        setShowPassword((prevState) => !prevState);
+    }
+
+    const inputType = type === 'password' && !showPassword ? 'password' : 'text';
+
+    return (
+        <div className="storybook-form-group">
+            <label htmlFor={type} className={'storybook-label'}>{label}</label>
+            <div className={'storybook-input-password'}>
+                <input
+                    type={inputType}
+                    id={type}
+                    value={type}
+                    className={['storybook-input', mode].join(' ')}
+                    required={required}
+                    {...props}
+                    onClick={onClick}
+                />
+                {type === 'password' && (
+                    <button
+                        type="button"
+                        className="storybook-button-password"
+                        onClick={passwordtoggle}
+                    >
+                        {showPassword ? 'Hide' : 'Show'}
+                    </button>
+                )}
+            </div>
+
+        </div>
+    );
+};
+
+Input.propTypes = {
+    type: PropTypes.string.isRequired,
+    required: PropTypes.bool,
+    error: PropTypes.bool,
+    label: PropTypes.string,
+    onClick: PropTypes.func,
+};
+
+Input.defaultProps = {
+    type: 'input',
+    required: false,
+}
