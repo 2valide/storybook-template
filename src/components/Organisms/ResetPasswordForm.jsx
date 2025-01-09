ed87025b5db32d5c97b4c2eaeaa3f63f1b72abe4
Hugo Pigree <116163096+HugoPigree@users.noreply.github.com>
2025-01-09 16:55:45 +0100

    page reset (#14)

---
+++ src/components/Organisms/ResetPasswordForm.jsx
@@ -0,0 +1,31 @@
+import {useState} from 'react'
+import '../../styles/ConnexionForm.css'
+import {Button} from "../molecules/button/Button.jsx";
+import {Input} from "../molecules/Textinput/Input.jsx";
+
+export function ResetPasswordForm() {
+    const [email, setEmail] = useState('')
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log('Login attempted with:', {email})
+    }
+
+    return (
+        <div className="form-container">
+            <form onSubmit={handleSubmit} className="login-form">
+                <h1>Reset password</h1>
+
+                <Input label={"Email"} type={"email"} required={true} value={email} onChange={(e) => setEmail(e.target.value)} />
+
+
+
+                <Button
+                    onClick={() => {}}
+                    label={"send"}
+                />
+            </form>
+        </div>
+    )
+}
