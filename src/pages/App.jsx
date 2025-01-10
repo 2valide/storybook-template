e37d06bc0e93d8f7d496643c85c079dfffdd56b9
h_pigree <h.pigreepro@gmail.com>
2025-01-10 12:15:57 +0100

    header et routes

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,14 +1,19 @@
-import '../styles/App.css'
+import '../styles/App.css';
 import Connection from "./Connection.jsx";
 import ResetPassword from "./ResetPassword.jsx";
+import Header from "./Header.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
     return (
-        <>
-            {/*<ResetPassword />*/}
-            <Connection/>
-        </>
-    )
+        <Router>
+            <Header />
+            <Routes>
+                <Route path="/connexion" element={<Connection />} />
+                <Route path="/resetpassword" element={<ResetPassword />} />
+            </Routes>
+        </Router>
+    );
 }
 
-export default App
+export default App;
