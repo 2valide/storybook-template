318a8af01f514bfb27aa8c92559ea9a63f394065
Hugo Pigree <116163096+HugoPigree@users.noreply.github.com>
2025-01-10 12:19:58 +0100

    header et routes (#21)
    Co-authored-by: h_pigree <h.pigreepro@gmail.com>

---
+++ src/components/Organisms/HeaderForm.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import '../../styles/header.css';
+import {Button} from "../molecules/button/Button.jsx";
+import { Link } from 'react-router-dom';
+
+export function Header() {
+    return (
+        <header className="header">
+            <div className="header-content">
+                <h1 className="header-title">Storybook</h1>
+                <nav className="header-nav">
+                    <Link to="/profil" className="header-button">Profil</Link>
+                    <Link to="/connexion" className="header-button">Connexion</Link>
+                    <Link to="/resetpassword" className="header-button">Reset Password</Link>
+                </nav>
+            </div>
+            <hr className="header-border" />
+        </header>
+    );
+}
---
+++ src/pages/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Header} from "../components/Organisms/HeaderForm.jsx";
+import '../styles/connection.css'
+
+const Connection = () => {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
+
+export default Connection
+
